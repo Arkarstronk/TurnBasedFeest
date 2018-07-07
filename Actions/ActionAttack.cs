066fa5b95e441500584338302853ff1f5576994c
BlockCat <zinoonomiwo@gmail.com>
2018-07-07 20:42:15 +0200

    Actions

---
+++ Actions/ActionAttack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using TurnBasedFeest.Actors;
+
+namespace TurnBasedFeest.Actions
+{
+    class ActionAttack : IAction
+    {
+
+        public IActionResult Execute(Actor source, Actor target)
+        {
+            int damage = 10;
+
+            target.health.actorCurrentHealth -= damage;
+            return new ActionResultAttack(damage);
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class ActionResultAttack : IActionResult
+    {
+        private int damageDone;
+
+        public ActionResultAttack(int damageDone)
+        {
+            this.damageDone = damageDone;
+        }
+    }
+}
---
+++ Actions/IAction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurnBasedFeest.Actors;
+
+namespace TurnBasedFeest.Actions
+{
+    interface IAction
+    {
+        /// <summary>
+        /// Execute the action,
+        /// Returns IActionResult that contains relevant data regarding the executed action.
+        /// </summary>
+        /// <returns></returns>
+        IActionResult Execute(Actor source, Actor target);
+
+        // Needed methods for action animations.
+        void Update(GameTime gameTime);
+        void Draw(GameTime gameTime);
+    }
+
+    interface IActionResult
+    {
+
+    }
+}
